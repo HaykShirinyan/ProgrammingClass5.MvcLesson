072b6d10dd9efb3c9b0cd5cce138d4c6b7d8835f
Hayk Shirinyan <hayk.shirinyan@developerpartners.com>
2024-11-03 09:43:01 -0800

    Initial commit

---
+++ ProgrammingClass5.MvcLesson/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProgrammingClass5.MvcLesson.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
