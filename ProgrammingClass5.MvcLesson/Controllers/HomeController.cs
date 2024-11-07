8e207a5834bb30c0f68cfc41a10f8a0f084ff44e
Samvel Barseghyan <samoboxx@mail.ru>
2024-11-07 23:58:36 +0400

    Im tnayin ashxatanq

--- ProgrammingClass5.MvcLesson/Controllers/HomeController.cs
+++ ProgrammingClass5.MvcLesson/Controllers/HomeController.cs
@@ -22,6 +22,14 @@ namespace ProgrammingClass5.MvcLesson.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
