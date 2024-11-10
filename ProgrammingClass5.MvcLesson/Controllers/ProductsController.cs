8e46beaaa22c9aeb73b1c15b7a2cf6345d0254ba
Hayk Shirinyan <hayk.shirinyan@developerpartners.com>
2024-11-10 12:02:10 -0800

    Added products page

---
+++ ProgrammingClass5.MvcLesson/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgrammingClass5.MvcLesson.Data;
+using ProgrammingClass5.MvcLesson.Models;
+
+namespace ProgrammingClass5.MvcLesson.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+
+        public ProductsController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> products = _dbContext.Products.ToList();
+
+            return View(products);
+        }
+    }
+}
