becefe72d6c3fd31afb13ebf9b1c425984082abf
martzuro <arturgevorgyan1131@gmail.com>
2024-11-14 01:25:50 +0400

    Add ProductTypes and Manufacturers pages

---
+++ ProgrammingClass5.MvcLesson/Controllers/ManufacturersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgrammingClass5.MvcLesson.Data;
+using ProgrammingClass5.MvcLesson.Models;
+
+namespace ProgrammingClass5.MvcLesson.Controllers
+{
+    public class ManufacturersController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+
+        public ManufacturersController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Manufacturer> manufacturers = _dbContext.Manufacturers.ToList();
+
+            return View(manufacturers);
+        }
+    }
+}
--- ProgrammingClass5.MvcLesson/Data/ApplicationDbContext.cs
+++ ProgrammingClass5.MvcLesson/Data/ApplicationDbContext.cs
@@ -8,6 +8,10 @@ namespace ProgrammingClass5.MvcLesson.Data
     {
         public DbSet<Product> Products { get; set; }
 
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
