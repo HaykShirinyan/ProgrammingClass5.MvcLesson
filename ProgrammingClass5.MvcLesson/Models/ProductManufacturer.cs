b2fd30fd5e302ebdbde5b6cfb580a3cbb63e2428
DoCAnnaArakelyan999 <artbygapi@gmail.com>
2024-11-15 21:29:48 +0400

    Add ProductManufacturer page

---
+++ ProgrammingClass5.MvcLesson/Models/ProductManufacturer.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProgrammingClass5.MvcLesson.Models
+{
+    public class ProductManufacturer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Adress { get; set; }  
+
+        [Required]
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public double Rating { get; set; }
+    }
+}
---
+++ ProgrammingClass5.MvcLesson/Models/ProductType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProgrammingClass5.MvcLesson.Models
+{
+    public class ProductType
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
