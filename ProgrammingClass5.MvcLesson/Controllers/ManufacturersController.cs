d27ee7d1c2b845ceb81faa2b0009b0a934c3af8d
Samvel Barseghyan <samoboxx@mail.ru>
2024-11-21 22:57:26 +0400

    Tnayin ashxatanq)

--- ProgrammingClass5.MvcLesson/Controllers/ManufacturersController.cs
+++ ProgrammingClass5.MvcLesson/Controllers/ManufacturersController.cs
@@ -11,11 +11,52 @@ namespace ProgrammingClass5.MvcLesson.Controllers
         {
             _dbContext = dbContext;
         }
-
+        [HttpGet]
         public IActionResult Index()
         {
             List<Manufacturer> manufacturers = _dbContext.Manufacturers.ToList();
             return View(manufacturers);
         }
+        [HttpGet]
+        public IActionResult Create() 
+        { 
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Manufacturer manufacturer)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Manufacturers.Add(manufacturer);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(manufacturer);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var manufacturer = _dbContext.Manufacturers.Find(id);
+            return View(manufacturer);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Manufacturer manufacturer)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Manufacturers.Update(manufacturer);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(manufacturer);
+        }
+
     }
 }
