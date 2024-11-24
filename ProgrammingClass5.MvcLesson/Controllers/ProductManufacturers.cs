3a72f674d478eb1db20263bb77753e262c4173fc
DoCAnnaArakelyan999 <artbygapi@gmail.com>
2024-11-24 18:32:59 +0400

    ProductManufacturers Added ProductTypes Added

--- ProgrammingClass5.MvcLesson/Controllers/ProductManufacturers.cs
+++ ProgrammingClass5.MvcLesson/Controllers/ProductManufacturers.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 using ProgrammingClass5.MvcLesson.Data;
+using ProgrammingClass5.MvcLesson.Data.Migrations;
 using ProgrammingClass5.MvcLesson.Models;
 
 namespace ProgrammingClass5.MvcLesson.Controllers
@@ -14,13 +16,71 @@ namespace ProgrammingClass5.MvcLesson.Controllers
         public ProductManufacturersController(ApplicationDbContext dbContext)
         {
             _dbContext = dbContext;
-        }   
+        }
 
+        [HttpGet]
         public IActionResult Index()
         {
             List<ProductManufacturer> productManufacturers = _dbContext.ProductManufacturers.ToList();
             return View(productManufacturers);
 
         }
+
+        [HttpGet]
+        public IActionResult Create()
+
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ProductManufacturer productManufacturer)
+        {
+            if (ModelState.IsValid)
+            {
+
+                _dbContext.ProductManufacturers.Add(productManufacturer);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+
+            }
+
+            return View(productManufacturer);
+        }
+
+
+            [HttpGet]
+             public IActionResult Edit(int id)
+
+            { 
+                var productManufacturer = _dbContext.ProductManufacturers.Find(id);
+                return View(productManufacturer);
+            
+            }
+
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+
+            public IActionResult Edit(ProductManufacturer productManufacturer) 
+        {
+            if (ModelState.IsValid)
+            { 
+                _dbContext.ProductManufacturers.Update(productManufacturer);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            
+            }
+
+            return View(productManufacturer);
+
+
+
+
+        }
+        
     }
 }
--- ProgrammingClass5.MvcLesson/Controllers/ProductTypes.cs
+++ ProgrammingClass5.MvcLesson/Controllers/ProductTypes.cs
@@ -20,5 +20,66 @@ namespace ProgrammingClass5.MvcLesson.Controllers
             List<ProductType> productTypes = _dbContext.ProductTypes.ToList();
             return View(productTypes);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ProductType productType)
+
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.ProductTypes.Add(productType);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+
+            }
+
+            return View(productType);
+        }
+
+        [HttpGet]
+
+        public IActionResult Edit(int id)
+
+        {
+            var productType = _dbContext.ProductTypes.Find(id);
+            return View(productType);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ProductType productType)
+
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.ProductTypes.Update(productType);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+
+            }
+
+            return View(productType);
+
+
+        }
+
+
+
+
+
+
+        
     }
 }
