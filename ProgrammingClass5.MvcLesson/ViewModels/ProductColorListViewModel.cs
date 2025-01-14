0848bd651f865cc40e103b94053424eff86687f9
DoCAnnaArakelyan999 <artbygapi@gmail.com>
2025-01-14 17:59:55 +0400

    ProducColorViewModel

---
+++ ProgrammingClass5.MvcLesson/ViewModels/ProductColorListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ProgrammingClass5.MvcLesson.Models;
+
+namespace ProgrammingClass5.MvcLesson.ViewModels
+{
+    public class ProductColorListViewModel
+    {
+        public int ProductId { get; set; }  
+        public List<ProductColor> ProductColors { get; set; }
+    }
+}
---
+++ ProgrammingClass5.MvcLesson/ViewModels/ProductColorViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ProgrammingClass5.MvcLesson.Models;
+
+namespace ProgrammingClass5.MvcLesson.ViewModels
+{
+    public class ProductColorViewModel
+    {
+        public ProductColor ProductColor { get; set; }
+        public List<Color> Colors { get; set; }
+    }
+}
